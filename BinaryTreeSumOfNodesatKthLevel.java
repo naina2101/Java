6f2f00ea3c562f74c5093966634cd9d8fdb8ccea
Sunaina Singh <pt.naina1@gmail.com>
2024-12-19 11:55:51 +0530

    Traversals added

--- BinaryTreeSumOfNodesatKthLevel.java
+++ BinaryTreeSumOfNodesatKthLevel.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class BinaryTreeSumOfNodesatKthLevel {
     static class Node{
         int data;
@@ -25,8 +27,75 @@ public class BinaryTreeSumOfNodesatKthLevel {
     }
 
     public static void main(String[] args) {
-        int[] node = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int[] Node = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
-        Node root = tree.buildTree(node);
+        Node root = tree.buildTree(Node);
+        System.out.println("Preorder traversal:");
+        preOrder(root);
+        System.out.println("\nInorder traversal:");
+        inOrder(root);
+        System.out.println("\nPostorder traversal:");
+        postOrder(root);
+        System.out.println("\nLevel order traversal:");
+        levelOrder(root);
+    }
+
+    //PreOrder Traversal
+    public static void preOrder(Node root){
+        if(root == null)
+            return;
+
+        System.out.print(root.data+ " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    //InOrder Traversal
+    public static void inOrder(Node root){
+        if(root == null)
+            return;
+
+        inOrder(root.left);
+        System.out.print(root.data+ " ");
+        inOrder(root.right);
+    }
+
+    //InOrder Traversal
+    public static void postOrder(Node root){
+        if(root == null)
+            return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data+ " ");
+    }
+
+    //LevelOrder Traversal
+    public static void levelOrder(Node root){
+        if(root == null)
+            return;
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if(currNode == null){
+                System.out.println();
+                if(q.isEmpty())
+                    break;
+                else
+                    q.add(null);
+
+            }
+            else{
+                System.out.print(currNode.data+ " ");
+                if(currNode.left != null)
+                    q.add(currNode.left);
+                if(currNode.right != null)
+                    q.add(currNode.right);
+            }
+        }
     }
 }
